2cd46847a2dde4d37db97699e59243e7467fe309
Jorge Pérez <jorge.perez.rojas@gmail.com>
2017-08-18 21:44:32 -0300

    parbypar generator

--- text_generator/src/lstm.py
+++ text_generator/src/lstm.py
@@ -30,10 +30,15 @@ time_pref = time.strftime('%y%m%d.%H%M') + '_'
 
 #################
 ## input files
+train_text_file = '../data/horoscopo_0600_0300_train.txt' #'../data/horoscopo_0600_0300_train.txt'
+val_text_file = '../data/horoscopo_0600_0300_val.txt' #'../data/horoscopo_0600_0300_val.txt'
+voc_file = '../data/horoscopo_0600_0300_voc.txt' #'../data/horoscopo_0600_0300_voc.txt'
 
-train_text_file = '../data/biblia_ntv_train.txt' #'../data/horoscopo_0600_0300_train.txt'
-val_text_file = '../data/biblia_ntv_val.txt' #'../data/horoscopo_0600_0300_val.txt'
-voc_file = '../data/biblia_ntv_voc.txt' #'../data/horoscopo_0600_0300_voc.txt'
+##### set parameters of the training process
+batch_size = 128
+epochs = 2
+patience = 4
+#####
 
 ##### MASK VALUE EQUALS 0
 ##### CONSIDER HAVING THE EMPTY STRING AS THE FIRST SYMBOL IN YOUR VOCABULARY TO MAKE THIS WORK, 
@@ -51,8 +56,8 @@ ind_train_tokens = [voc_ind[token] for token in train_tokens if token in voc]
 ind_val_tokens = [voc_ind[token] for token in val_tokens if token in voc]
 
 ### for testing pourposes you can use a small subset of the train or val data
-max_train_data =  len(ind_train_tokens)
-max_val_data = len(ind_val_tokens)
+max_train_data =  200 #len(ind_train_tokens)
+max_val_data = 200 #len(ind_val_tokens)
 ind_train_tokens = ind_train_tokens[:max_train_data]
 ind_val_tokens = ind_val_tokens[:max_val_data]
 
@@ -69,12 +74,6 @@ dropout = 0.3
 rec_dropout = 0.3
 optimizer = 'adam'
 impl = 2 # BE CAREFULL!!! must be 2 for GPU
-l2reg=0.01
-#####
-
-##### set parameters of the training process
-batch_size = 256
-epochs = 40
 #####
 
 if retraining == True:
@@ -85,11 +84,9 @@ else:
     lstm_model = Sequential()
     lstm_model.add(Masking(mask_value=mask_value, input_shape=(max_len, len(voc))))
     lstm_model.add(LSTM(lstm_units, recurrent_dropout=rec_dropout, 
-        kernel_regularizer=l2(l2reg),
         implementation=impl, return_sequences=True))
     lstm_model.add(Dropout(dropout))
     lstm_model.add(LSTM(lstm_units, recurrent_dropout=rec_dropout, 
-        kernel_regularizer=l2(l2reg),
         implementation=impl)) # , return_sequences=True))
     # lstm_model.add(Dropout(dropout/4))
     # lstm_model.add(LSTM(lstm_units, recurrent_dropout=rec_dropout/4, implementation=impl))
@@ -100,10 +97,10 @@ else:
 
 lstm_model.summary()
 
-
-outfile = out_model_pref + time_pref + \
-    'bs{0:03d}_'.format(batch_size) + \
-    '{loss:.2f}_{val_loss:.2f}_{val_top_k_categorical_accuracy:.2f}_{epoch:03d}.h5'
+outfile = out_model_pref + time_pref + '.h5'
+#outfile = out_model_pref + time_pref + \
+#    'bs{0:03d}_'.format(batch_size) + \
+#    '{loss:.2f}_{val_loss:.2f}_{val_top_k_categorical_accuracy:.2f}_{epoch:03d}.h5'
 
 checkpoint = ModelCheckpoint(
     out_directory_model + outfile, 
@@ -114,13 +111,13 @@ checkpoint = ModelCheckpoint(
 )
 
 early_stopping = EarlyStopping(
-    monitor='val_top_k_categorical_accuracy', min_delta=0, patience=4, verbose=0, mode='auto')
+    monitor='val_top_k_categorical_accuracy', min_delta=0, patience=patience, verbose=0, mode='auto')
 
 ### create the generator objects
-#train_gen = ParByParGenerator(batch_size, ind_train_tokens, voc, max_len, voc_ind[nl_symbol], paragraphs_to_join = 2, mask_value = mask_value)
-#val_gen = ParByParGenerator(batch_size, ind_val_tokens, voc, max_len, voc_ind[nl_symbol], paragraphs_to_join = 2, mask_value = mask_value)
-train_gen = ContinuousGenerator(batch_size, ind_train_tokens, voc, max_len)
-val_gen = ContinuousGenerator(batch_size, ind_val_tokens, voc, max_len)
+train_gen = ParByParGenerator(batch_size, ind_train_tokens, voc, max_len, voc_ind[nl_symbol], paragraphs_to_join = 2, mask_value = mask_value)
+val_gen = ParByParGenerator(batch_size, ind_val_tokens, voc, max_len, voc_ind[nl_symbol], paragraphs_to_join = 2, mask_value = mask_value)
+#train_gen = ContinuousGenerator(batch_size, ind_train_tokens, voc, max_len)
+#val_gen = ContinuousGenerator(batch_size, ind_val_tokens, voc, max_len)
 
 print('steps per epoch training:', train_gen.steps_per_epoch)
 print('steps per epoch validation:', val_gen.steps_per_epoch)
@@ -135,23 +132,38 @@ model_output = lstm_model.fit_generator(
 )
 
 # save also the last state (to continue training if needed)
-final_model_file = out_directory_model + 'final_' + out_model_pref + time_pref + \
-    'bs{0:03d}'.format(batch_size) + '.h5'
+final_model_file = out_directory_model + 'final_' + out_model_pref + time_pref + '.h5'
 print('saving last model:', final_model_file)
 lstm_model.save(final_model_file)
 
 # save history
-outfile_history = out_directory_train_history + out_model_pref + time_pref + \
-    'bs{0:03d}'.format(batch_size) + '.txt'
-
-outfile_architecture = out_directory_train_history + out_model_pref + time_pref + 'arch.yaml'
-
+outfile_history = out_directory_train_history + out_model_pref + time_pref + 'hist.txt'
+outfile_parameters = out_directory_train_history + out_model_pref + time_pref + 'pars.txt'
 
 print('saving history:', outfile_history)
+print('saving parameters:', outfile_parameters)
 with open(outfile_history,'w') as out: 
     out.write(str(model_output.history))
     out.write('\n')
 
-# TODO: save a file with all the parameter configurations! (yaml stores almost everything but train set plus bs y also needed for comparisons)
-with open(outfile_architecture,'w') as out:
-    out.write(lstm_model.to_yaml())
+# save a file with all the parameter configurations! (yaml stores almost everything but train set plus bs y also needed for comparisons)
+with open(outfile_parameters,'w') as out:
+    out.write('training parameters\n\n')
+    out.write('train file: ')
+    out.write(train_text_file)
+    out.write('\nvalidation file: ')
+    out.write(val_text_file)
+    out.write('\nvoc_file: ')
+    out.write(voc_file)
+    out.write('\nbatch size: ')
+    out.write(str(batch_size))
+    out.write('\nepochs: ')
+    out.write(str(epochs))
+    out.write('\n\n')
+    if retraining == True:
+        out.write('retraining model: ')
+        out.write(previous_model_file)
+    else:
+        out.write('model specs: \n\n')
+        out.write(lstm_model.to_yaml())
+
