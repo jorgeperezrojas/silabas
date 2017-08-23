add1d56164cc61f67e9e454d4168cbf21e2bf674
Jorge Pérez <jorge.perez.rojas@gmail.com>
2017-08-22 22:20:25 -0300

    last version

--- text_generator/src/lstm_predict.py
+++ text_generator/src/lstm_predict.py
@@ -13,7 +13,7 @@ voc_file = '../data/horoscopo_5000_0300_voc.txt'
 voc = open(voc_file).read().split()
 voc_ind = dict((s,i) for i,s in enumerate(voc))
 
-model_file = '../models/lstm_model_170819.0143.h5'
+model_file = '../models/lstm_model_170821.0948.h5'
 model = load_model(model_file)
 predictor = utils.PredictorParByParReal(model,voc,voc_ind,voc_ind['<nl>'])
 s = predictor.generate_text(mode='interactive')
